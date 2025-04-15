d5cd27966ddd14134b0143a81a59ae144dcb2b04
GoodVoid <98227501+GoodVoid@users.noreply.github.com>
2025-04-15 18:05:57 +0300

    Some Design things
    Add Resourse Dictionarys
    Some new styles
    NavigationVM

---
+++ MyLinx/MyLinx/ViewModel/NavigationVM.cs
@@ -0,0 +1,14 @@
+﻿using MyLinx.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinx.Model
+{
+    class NavigationVM :ViewModelBase
+    {
+
+    }
+}
